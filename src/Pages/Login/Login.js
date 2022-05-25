23e8616b9d80ee80dabe11d6a99f3601a09ee801
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-05-25 17:15:08 +0600

    add register from

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -5,10 +5,10 @@ const Login = () => {
     return (
         <div className='login mb-20'>
             <h1 className='text-primary text-3xl font-bold mb-10'>Login</h1>
-            <input type="text" placeholder="Type here" className="input input-bordered input-error w-full max-w-xs" /> <br />
-            <input type="text" placeholder="Type here" className="input input-bordered input-error w-full max-w-xs" /> <br />
-            <button className="btn btn-active btn-primary">Button</button>
-            <p>Already have a account? <span className='text-primary'><Link to='/register'>Register Please</Link></span></p>
+            <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" /> <br />
+            <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" /> <br />
+            <button className="btn btn-active btn-primary">Login</button>
+            <p>No Account <span className='text-primary'><Link to='/register'>Register Please</Link></span></p>
         </div>
     );
 };
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import './Register.css';
 const Register = () => {
     return (
-        <div>
+        <div className='register mb-10'>
             <h1 className='text-primary text-3xl font-bold mb-10'>Register</h1>
+            <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" /> <br />
+            <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" /> <br />
+            <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" /> <br />
+            <button className="btn btn-active btn-primary">Register</button>
+            <p>Already have a account? <span className='text-primary'><Link to='/login'>Login Please</Link></span></p>
         </div>
     );
 };
