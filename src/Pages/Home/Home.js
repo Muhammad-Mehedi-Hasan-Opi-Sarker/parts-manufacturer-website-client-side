2942f52c4fb49c59d07ee04611058409cbf2d822
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-05-25 16:15:11 +0600

    add home in font of input for email

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -13,6 +13,7 @@ const Home = () => {
             <Summary></Summary>
             <AboutUs></AboutUs>
             <Reviews></Reviews>
+        
         </div>
        
     );
--- src/Pages/Home/Reviews/Reviews.js
+++ src/Pages/Home/Reviews/Reviews.js
@@ -15,139 +15,146 @@ const Reviews = () => {
         <div>
             <h1 className='text-3xl font-bold'>Customers Says</h1>
             <div className='px-12 grid lg:grid-cols-5 sm:grid-cols-1 p-3'>
-            <div style={{ width: '240px', height: '300px' }} class="card opi mb-3 w-96 bg-primary text-primary-content">
-                {/* avatar  */}
-                <div class="avatar">
-                    <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                        <img src={picture1} alt='' />
+                <div style={{ width: '240px', height: '300px' }} className="card opi mb-3 w-96 bg-primary text-primary-content">
+                    {/* avatar  */}
+                    <div className="avatar">
+                        <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                            <img src={picture1} alt='' />
+                        </div>
+                    </div>
+                    <div className="card-body">
+                        <h2 className="card-title">Boris Jonson</h2>
+                        <p>The products here are very good. They deliver the product very
+                            quickly. I already buy the products. </p>
+
                     </div>
                 </div>
-                <div class="card-body">
-                    <h2 class="card-title">Boris Jonson</h2>
-                    <p>The products here are very good. They deliver the product very
-                        quickly. I already buy the products. </p>
-                    
-                </div>
-            </div>
-            <div  style={{ width: '240px', height: '300px',padding:'9px' }} class="card opi w-96 bg-primary text-primary-content">
-                <div class="avatar">
-                    <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                        <img src={picture2} alt='' />
+                <div style={{ width: '240px', height: '300px', padding: '9px' }} className="card opi w-96 bg-primary text-primary-content">
+                    <div className="avatar">
+                        <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                            <img src={picture2} alt='' />
+                        </div>
+                    </div>
+                    <div className="card-body">
+                        <h2 className="card-title">Adam</h2>
+                        <p>The products here are of very high quality. People in
+                            my country love this company's products.</p>
+
                     </div>
                 </div>
-                <div class="card-body">
-                    <h2 class="card-title">Adam</h2>
-                    <p>The products here are of very high quality. People in 
-                        my country love this company's products.</p>
-                   
-                </div>
-            </div>
-            <div  style={{ width: '240px', height: '300px',padding:'9px' }} class="card opi w-96 bg-primary text-primary-content">
-                <div class="avatar">
-                    <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                        <img src={picture11} alt=''/>
+                <div style={{ width: '240px', height: '300px', padding: '9px' }} className="card opi w-96 bg-primary text-primary-content">
+                    <div className="avatar">
+                        <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                            <img src={picture11} alt='' />
+                        </div>
+                    </div>
+                    <div className="card-body">
+                        <h2 className="card-title">Messi</h2>
+                        <p>I import products to my country every year from now on.
+                            The products here are very good</p>
+
                     </div>
                 </div>
-                <div class="card-body">
-                    <h2 class="card-title">Messi</h2>
-                    <p>I import products to my country every year from now on.
-                         The products here are very good</p>
-                    
-                </div>
-            </div>
-            <div style={{ width: '240px', height: '300px',padding:'9px' }} class="card opi w-96 bg-primary text-primary-content">
-                <div class="avatar">
-                    <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                        <img src={picture4} alt=''/>
+                <div style={{ width: '240px', height: '300px', padding: '9px' }} className="card opi w-96 bg-primary text-primary-content">
+                    <div className="avatar">
+                        <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                            <img src={picture4} alt='' />
+                        </div>
+                    </div>
+                    <div className="card-body">
+                        <h2 className="card-title">Jonson</h2>
+                        <p>The main carriers here are very good, and their products are
+                            also very good. They can be trusted</p>
+
                     </div>
                 </div>
-                <div class="card-body">
-                    <h2 class="card-title">Jonson</h2>
-                    <p>The main carriers here are very good, and their products are 
-                        also very good. They can be trusted</p>
-                   
-                </div>
-            </div>
-            <div style={{ width: '240px', height: '300px',padding:'9px' }} class="card opi w-96 bg-primary text-primary-content">
-                <div class="avatar">
-                    <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                        <img src={picture5} alt=''/>
+                <div style={{ width: '240px', height: '300px', padding: '9px' }} className="card opi w-96 bg-primary text-primary-content">
+                    <div className="avatar">
+                        <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                            <img src={picture5} alt='' />
+                        </div>
+                    </div>
+                    <div className="card-body">
+                        <h2 className="card-title">Harry Protal</h2>
+                        <p>I import products to my country every year from now on.
+                            The products here are very good poducts .</p>
+
                     </div>
                 </div>
-                <div class="card-body">
-                    <h2 class="card-title">Harry Protal</h2>
-                    <p>I import products to my country every year from now on.
-                         The products here are very good poducts .</p>
-                    
-                </div>
-            </div>
-            <div style={{ width: '240px', height: '300px',padding:'9px' }} class="card opi w-96 bg-primary text-primary-content">
-                <div class="avatar">
-                    <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                        <img src={picture6} alt='' />
+                <div style={{ width: '240px', height: '300px', padding: '9px' }} className="card opi w-96 bg-primary text-primary-content">
+                    <div className="avatar">
+                        <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                            <img src={picture6} alt='' />
+                        </div>
+                    </div>
+                    <div className="card-body">
+                        <h2 className="card-title">Ronaldo</h2>
+                        <p>I import products to my country every year from now on.
+                            The products here are very good</p>
+
                     </div>
                 </div>
-                <div class="card-body">
-                    <h2 class="card-title">Ronaldo</h2>
-                    <p>I import products to my country every year from now on.
-                         The products here are very good</p>
-                    
-                </div>
-            </div>
-            <div style={{ width: '240px', height: '300px',padding:'9px' }} class="card opi w-96 bg-primary text-primary-content">
-                <div class="avatar">
-                    <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                        <img src={picture6} alt='' />
+                <div style={{ width: '240px', height: '300px', padding: '9px' }} className="card opi w-96 bg-primary text-primary-content">
+                    <div className="avatar">
+                        <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                            <img src={picture6} alt='' />
+                        </div>
+                    </div>
+                    <div className="card-body">
+                        <h2 className="card-title">Deniel</h2>
+                        <p>The products here are very good. They deliver the product very
+                            quickly. I already buy the products.</p>
+
                     </div>
                 </div>
-                <div class="card-body">
-                    <h2 class="card-title">Deniel</h2>
-                    <p>The products here are very good. They deliver the product very
-                        quickly. I already buy the products.</p>
-                    
-                </div>
-            </div>
-            <div style={{ width: '240px', height: '300px',padding:'9px' }} class="card opi w-96 bg-primary text-primary-content">
-                <div class="avatar">
-                    <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                        <img src={picture7} alt='' />
+                <div style={{ width: '240px', height: '300px', padding: '9px' }} className="card opi w-96 bg-primary text-primary-content">
+                    <div className="avatar">
+                        <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                            <img src={picture7} alt='' />
+                        </div>
+                    </div>
+                    <div className="card-body">
+                        <h2 className="card-title">Robin</h2>
+                        <p>The products here are of very high quality. People in
+                            my country love this company's products.</p>
+
                     </div>
                 </div>
-                <div class="card-body">
-                    <h2 class="card-title">Robin</h2>
-                    <p>The products here are of very high quality. People in 
-                        my country love this company's products.</p>
-                    
-                </div>
-            </div>
-            <div style={{ width: '240px', height: '300px',padding:'9px' }} class="card opi w-96 bg-primary text-primary-content">
-                <div class="avatar">
-                    <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                        <img src={picture8} alt='' />
+                <div style={{ width: '240px', height: '300px', padding: '9px' }} className="card opi w-96 bg-primary text-primary-content">
+                    <div className="avatar">
+                        <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                            <img src={picture8} alt='' />
+                        </div>
+                    </div>
+                    <div className="card-body">
+                        <h2 className="card-title">Yeadov</h2>
+                        <p>I am very proud to work with them. They are v
+                            ery careful about their products. That's why I b
+                            uy products from their work</p>
+
                     </div>
                 </div>
-                <div class="card-body">
-                    <h2 class="card-title">Yeadov</h2>
-                    <p>I am very proud to work with them. They are v
-                        ery careful about their products. That's why I b
-                        uy products from their work</p>
-                    
-                </div>
-            </div>
-            <div style={{ width: '240px', height: '300px',padding:'9px' }} class="card opi w-96 bg-primary text-primary-content">
-                <div class="avatar">
-                    <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                        <img src={picture9} alt=''/>
+                <div style={{ width: '240px', height: '300px', padding: '9px' }} className="card opi w-96 bg-primary text-primary-content">
+                    <div className="avatar">
+                        <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                            <img src={picture9} alt='' />
+                        </div>
+                    </div>
+                    <div className="card-body">
+                        <h2 className="card-title">Hasan</h2>
+                        <p>The products of this company are of very high quality. I regular
+                            ly buy these products from now on. You have to trust them</p>
+
                     </div>
                 </div>
-                <div class="card-body">
-                    <h2 class="card-title">Hasan</h2>
-                    <p>The products of this company are of very high quality. I regular
-                        ly buy these products from now on. You have to trust them</p>
-                    
-                </div>
             </div>
-        </div>
+            <div className="mt-16 mb-16">
+                <h1 className='text-2xl font-bold mb-5'>Get our latest news and special sales</h1>
+                <p className='mb-5'>You may unsubscribe at any moment. For that purpose, please 
+                    find our contact info in the legal notice.</p>
+                <input type="text" placeholder="Email Addres" className="input input-bordered w-full max-w-xs" />
+                <button className="btn btn-primary ml-3">Get</button>
+            </div>
         </div>
     );
 };
