eab107122995ca579d6bdd10edc1a35affd5bc51
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-05-28 14:13:17 +0600

    myorder add and show table data

--- src/Pages/Dhashboard/MyOrder.js
+++ src/Pages/Dhashboard/MyOrder.js
@@ -3,19 +3,43 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 
 const MyOrder = () => {
-    const [user] =useAuthState(auth);
-    const [myorder,setOrder] = useState([]);
-    useEffect(()=>{
+    const [user] = useAuthState(auth);
+    const [myorder, setOrder] = useState([]);
+    useEffect(() => {
         if (user) {
             fetch(`http://localhost:5000/booking?customerEmail=${user.email}`)
-            .then(res=>res.json())
-            .then(data=>setOrder(data))
+                .then(res => res.json())
+                .then(data => setOrder(data))
         }
-    },[])
+    }, [])
     return (
-        
-        <div>
-            <h1>My order {myorder?.length}</h1>
+
+        <div class="overflow-x-auto">
+            <table class="table w-full">
+
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>My Product</th>
+                        <th>Order Quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        myorder.map((my,index)=><tr>
+                            <th>{index}</th>
+                            <td>{my.customerName}</td>
+                            <td>{my.customerEmail}</td>
+                            <td>{my.productName}</td>
+                            <td>{my.order}</td>
+                        </tr>)
+                    }
+                    
+                   
+                </tbody>
+            </table>
         </div>
     );
 };
